9df06812ac227599c8f3d2fdc3bd93dd40532941
SilviaRahma10 <silviarahma270201@gmail.com>
2022-09-17 21:46:11 +0700

    membuat pembagian role

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,23 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::group(['middleware' => ['auth', 'role:admin']], function() {
+    Route::get('/admin', function() {
+
+    });
+
+});
+
+Route::group(['middleware' => ['auth', 'role:koordinator']], function() {
+
+    Route::get('/Koordinator', function() {
+
+    });
+});
+
+Route::group(['middleware' => ['auth', 'role:mahasiswa']], function() {
+    Route::get('/mahasiswa', function() {
+
+    });
+});
